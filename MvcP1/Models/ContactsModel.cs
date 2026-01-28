180395e3aec45806a7ee0c9d6d75ccf37f83db3a
ankelkoss <70823492+ankelkoss@users.noreply.github.com>
2026-01-28 15:28:24 +0200

    Create ContactsModel.cs

---
+++ MvcP1/Models/ContactsModel.cs
@@ -0,0 +1,11 @@
+﻿namespace MvcP1.Models
+{
+    public class ContactsModel : BaseModel
+    {
+        public string Name { get; set; } = null!;
+        public string Phone { get; set; } = null!;
+        public string? PhoneAlt { get; set; }
+        public string? Email { get; set; }
+        public string? DescShort { get; set; }
+    }
+}
