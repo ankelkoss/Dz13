06eb69a46f95f1446e97dcb6bda4dfba58abf73d
ankelkoss <70823492+ankelkoss@users.noreply.github.com>
2026-01-28 15:28:30 +0200

    Create NoteModel.cs

---
+++ MvcP1/Models/NoteModel.cs
@@ -0,0 +1,9 @@
+﻿namespace MvcP1.Models
+{
+    public class NoteModel : BaseModel
+    {
+        public string Name { get; set; } = null!;
+        public string Text { get; set; } = null!;
+        public List<string> Tags { get; set; } = new();
+    }
+}
