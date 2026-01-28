8ffab6d42cf8bb0b1701dc81c489f1568c366e10
ankelkoss <70823492+ankelkoss@users.noreply.github.com>
2026-01-28 15:28:22 +0200

    Create NoteModel.cs

---
+++ MvcP1/Models/BaseModel.cs
@@ -0,0 +1,9 @@
+﻿namespace MvcP1.Models
+{
+    public class BaseModel
+    {
+        public int Id { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+    }
+}
