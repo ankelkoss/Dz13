b171debe0bce26d40bb2299287e6f715e119270d
ankelkoss <70823492+ankelkoss@users.noreply.github.com>
2026-01-29 11:02:07 +0200

    Create ContactsConfiguration.cs

---
+++ MvcP1/Data/EntityConfigurations/ContactsConfiguration.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using MvcP1.Models;
+
+namespace MvcP1.Data.EntityConfigurations
+{
+    public class ContactsConfiguration : IEntityTypeConfiguration<Contacts>
+    {
+        public void Configure(EntityTypeBuilder<Contacts> builder)
+        {
+            builder.Property(x => x.Name)
+                .HasMaxLength(30) // Степан Работа
+                .IsRequired();
+
+            builder.Property(x => x.Phone)
+               .HasMaxLength(13) // +380931234567
+               .IsRequired();
+
+            builder.Property(x => x.PhoneAlt)
+               .HasMaxLength(13); // +380931234567
+
+            builder.Property(x => x.Email)
+                .HasMaxLength(50);
+
+            builder.Property(x => x.DescShort)
+               .HasMaxLength(100);
+
+            builder.Property(x => x.CreatedAt)
+                 .HasDefaultValueSql("SYSUTCDATETIME()");
+
+            builder.Property(x => x.UpdatedAt)
+                .HasDefaultValueSql("SYSUTCDATETIME()");
+
+            builder.ToTable("Contacts", tb => tb
+                .HasTrigger("TR_Contacts_UpdatedAt"));
+        }
+    }
+}
