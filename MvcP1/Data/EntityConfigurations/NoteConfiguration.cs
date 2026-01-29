8fd5b069f9d51cd05ba66b706e8991f3b4a031cf
ankelkoss <70823492+ankelkoss@users.noreply.github.com>
2026-01-29 11:02:11 +0200

    Create NoteConfiguration.cs

---
+++ MvcP1/Data/EntityConfigurations/NoteConfiguration.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+using MvcP1.Models;
+using System.Text.Json;
+
+namespace MvcP1.Data.EntityConfigurations
+{
+    public class NoteConfiguration : IEntityTypeConfiguration<Note>
+    {
+        public void Configure(EntityTypeBuilder<Note> builder)
+        {
+            var converter = new ValueConverter<List<string>, string>(
+                v => JsonSerializer.Serialize(v ?? new List<string>(), (JsonSerializerOptions?)null),
+                v => string.IsNullOrWhiteSpace(v)
+                    ? new List<string>()
+                    : JsonSerializer.Deserialize<List<string>>(v, (JsonSerializerOptions?)null) ?? new List<string>()
+            );
+
+            var comparer = new ValueComparer<List<string>>(
+                (c1, c2) => (c1 ?? new()).SequenceEqual(c2 ?? new()),
+                c => (c ?? new()).Aggregate(0, (a, v) => HashCode.Combine(a, v.GetHashCode())),
+                c => (c ?? new()).ToList()
+            );
+
+            builder.Property(x => x.Name)
+                .HasMaxLength(100)
+                .IsRequired();
+
+            builder.Property(x => x.Text)
+                .HasMaxLength(1000)
+                .IsRequired();
+
+            builder.Property(e => e.Tags)
+                .HasConversion(converter)
+                .HasColumnType("nvarchar(max)")
+                .Metadata.SetValueComparer(comparer);
+
+            builder.Property(x => x.CreatedAt)
+                .HasDefaultValueSql("SYSUTCDATETIME()");
+
+            builder.Property(x => x.UpdatedAt)
+                .HasDefaultValueSql("SYSUTCDATETIME()");
+
+            builder.ToTable("Notes", tb => tb
+                .HasTrigger("TR_Notes_UpdatedAt"));
+        }
+    }
+}
