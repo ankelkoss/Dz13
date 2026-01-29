3566f95a99104f908529f4311866b43332e596c3
ankelkoss <70823492+ankelkoss@users.noreply.github.com>
2026-01-29 11:02:30 +0200

    create migrations

--- MvcP1/Migrations/20260129085731_InitialCreate.Designer.cs
+++ MvcP1/Migrations/20260129085731_InitialCreate.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using MvcP1.Data;
 
@@ -11,9 +12,11 @@ using MvcP1.Data;
 namespace MvcP1.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260129085731_InitialCreate")]
+    partial class InitialCreate
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
